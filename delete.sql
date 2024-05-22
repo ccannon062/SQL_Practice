faf444664b9ea51e77889b047c3b9bb645a2ac04
Caleb C <118626040+ccannon062@users.noreply.github.com>
2024-05-22 17:06:28 -0500

    Add files via upload

---
+++ delete.sql
@@ -0,0 +1,16 @@
+-- DELETE TABLES FROM DB
+
+CREATE DATABASE example;
+
+USE example;
+
+CREATE TABLE test (
+id INT auto_increment PRIMARY KEY,
+name VARCHAR(30),
+age INT
+);
+
+DESCRIBE test;
+SHOW TABLES;
+
+DROP TABLE test;
